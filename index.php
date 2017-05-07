3e36d6f66f6366890d0e8f80f5695d31756d3153
Rodrigocoronel <rodrigo.coronel.14@gmail.com>
2017-05-06 17:42:29 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chat</title>
+</head>
+<body>
+<form action="login.php" method="post">
+	E-mail: <input type="email" name="email"><br>
+	Contraseña: <input type="password" name="password"><br> 
+	<input type="submit" value="Iniciar sesión">
+	<button type="submit" formaction="registro.php">Registrarse</button>
+</form>
+</body>
+</html>
